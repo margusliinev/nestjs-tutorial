34cfe0d9672aa7348c706b9c231a48d1227539d4
Margus Liinev <margus.liinev@gmail.com>
2023-08-09 23:34:36 +0300

    setup docker and prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    register() {
+        return this.authService.register();
+    }
+
+    @Post('login')
+    login() {
+        return this.authService.login();
+    }
+
+    @Post('logout')
+    logout() {
+        return this.authService.logout();
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    register() {
+        return `This action registers a user`;
+    }
+
+    login() {
+        return `This action logs a user in`;
+    }
+
+    logout() {
+        return `This action logs a user out`;
+    }
+}
